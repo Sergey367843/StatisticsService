e672e156256e758719c7dd5e7ddee48d4c9838e1
Sergey Garin <sdadqdq@gmail.com>
2025-10-09 23:29:39 +0300

    add new tests and change CI on GitHub

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -19,15 +19,26 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    void findMaxFromMiddle() {
+    void findMaxWhenMaxIsInMiddle() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 30, 8, 6, 11, 11, 12};
-        long expected = 30;
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 35, 8, 6, 11, 11, 12};
+        long expected = 35;
 
         long actual = service.findMax(incomesInBillions);
 
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    void findMaxWhenMaxIsLast() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 20};
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
