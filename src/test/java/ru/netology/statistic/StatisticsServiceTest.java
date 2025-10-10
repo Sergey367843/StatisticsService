c07d37426c944176e25e4fdf552eccc1f9205070
Sergey Garin <sdadqdq@gmail.com>
2025-10-10 13:15:45 +0300

    commit 2314125

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,28 +17,4 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    void findMaxWhenMaxIsInMiddle() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 35, 8, 6, 11, 11, 12};
-        long expected = 35;
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMaxWhenMaxIsLast() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 20};
-        long expected = 20;
-
-        long actual = service.findMax(incomesInBillions);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
